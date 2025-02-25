c3c058e1385bda417e2ccfb078f68b9b5b54e910
black-cat-developer <rahulsrichunar@gmail.com>
2025-02-25 19:01:30 +0530

    first day of python

---
+++ 1_style.py
@@ -0,0 +1,20 @@
+# addition in scripting style
+print("Hello world")
+a = 1
+b=56;
+print(a+b);
+
+# 1)python is dynamically typed language as programmer do not have to mention the data type
+'''
+above see multiple line comment
+data type allocated  by python 
+based on value stored into 
+variable
+'''
+# 2) It is most concise programming language means in minimum words we will do maximum task
+
+# addition in procedural style
+def add(a,b):
+  print(a+b);
+
+add(10,20);
