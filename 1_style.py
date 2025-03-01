578605358392512545185d7b2d4ada68df74c299
black-cat-developer <rahulsrichunar@gmail.com>
2025-03-01 23:13:31 +0530

    updated

--- 1_style.py
+++ 1_style.py
@@ -33,3 +33,24 @@ print('B')
 # do you want to print in same line ?
 print('A' , end='-')
 print('B')
+
+a = 10
+print(a, type(a)) #10 <class 'int'>
+
+b = 12.33
+print(b, type(b)) #12.33 <class 'float'>
+
+c = 'K'
+print(c, type(c)) #K <class 'str'>
+
+d = 'Code'
+print(d, type(d)) #Code <class 'str'>
+
+e = True
+print(e, type(e)) #True <class 'bool'>
+
+h = 3+5j
+print(h,type(h)) #(3+5j) <class 'complex'>
+
+s1="Rahul"
+print(len(s1))
